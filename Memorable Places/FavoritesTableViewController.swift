21440d40b4fe5c289f98a3d4a47f52368aaee77b
Gerardo Camilo <gerardo.camilo@gmail.com>
2015-06-20 20:26:39 -0400

    First app version : )

---
+++ Memorable Places/FavoritesTableViewController.swift
@@ -0,0 +1,93 @@
+//
+//  FavoritesTableViewController.swift
+//  Memorable Places
+//
+//  Created by Gerardo Camilo on 14/6/15.
+//  Copyright (c) 2015 ___GRCS___. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+var favoritePlaces = Dictionary<Int, MKPointAnnotation>();
+
+class FavoritesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        self.tableView.reloadData();
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        // println("numberOfRowsInSection: \(favoritePlaces.count)");
+        return favoritePlaces.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        //let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
+        
+        // print("IndexPathRow: \(indexPath.row).");
+        // print("FavoritePlaces Count: \(favoritePlaces.count).");
+        
+        // Configure the cell...
+        if(favoritePlaces.count > 0){
+
+            var annotation: MKPointAnnotation = favoritePlaces[indexPath.row]!
+            cell.textLabel?.text = annotation.title
+            // print("AnnotationDesc: \(annotation.title).");
+        }
+        
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        
+        annotationToDisplay = favoritePlaces[indexPath.row]!
+        
+        self.performSegueWithIdentifier("showPlaceOnMap", sender: self)
+        
+        return indexPath
+    }
+
+   
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Memorable Places/ViewController.swift
+++ Memorable Places/ViewController.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+var annotationToDisplay: MKPointAnnotation? = MKPointAnnotation()
+var displayList = Dictionary<Int, MKPointAnnotation>();
 
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        var uilpgr = UILongPressGestureRecognizer(target: self, action: "addFavoritePlace:")
+        uilpgr.minimumPressDuration = 1
+        map.addGestureRecognizer(uilpgr)
+        
+        if annotationToDisplay != nil
+        {
+            map.addAnnotation(annotationToDisplay!)
+        }
+        
+        
     }
 
+    func addFavoritePlace(gestureRecognizer: UILongPressGestureRecognizer){
+    
+        var touchPoint = gestureRecognizer.locationInView(self.map)
+        var newCoordinate: CLLocationCoordinate2D = map.convertPoint(touchPoint, toCoordinateFromView: self.map)
+        
+        
+        var annotation = MKPointAnnotation();
+        annotation.title = "Location \(favoritePlaces.count + 1)";
+        annotation.subtitle = "Added by you";
+        annotation.coordinate = newCoordinate;
+        
+        map.addAnnotation(annotation);
+        favoritePlaces[favoritePlaces.count] = annotation
+
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        annotationToDisplay = nil
+    }
+    
+    override func willMoveToParentViewController(parent: UIViewController?) {
+        
+        if parent == nil {
+         annotationToDisplay = nil
+        }
+
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
